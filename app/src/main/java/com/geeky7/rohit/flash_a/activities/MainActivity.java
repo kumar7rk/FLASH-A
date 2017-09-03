ea84f61586ae382accc81c6bcdbb9fd965bb2935
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-04 09:15:12 +0930

    MyApplication added as application name in manifest

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/MainActivity.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package com.geeky7.rohit.flash_a.activities;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -24,16 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         checkPermission();
-        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+//        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 //        boolean enabled = manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             message = extras.getString("Message");
             sender = extras.getString("Sender");
             Log.i("Text","Text is:"+ message);
 
-//            if ("Where".equals(message)) {
-              if(message.contains("Where")){
+            if ("Where".equals(message)) {
+//              if(message.contains("Where")){
                 Main.showToast("Location requested");
                 // this message initiated the location service which fetches the location and converts into an address
                 startService();
@@ -49,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
     private void startService() {
         Intent serviceIntent = new Intent(getApplicationContext(), LocationService.class);
         startService(serviceIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
