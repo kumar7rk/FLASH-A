2412561ce22e40d646cbdcd98b27b47bc0a69fe4
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-04 08:33:52 +0930

    SMSReceiver added

---
+++ app/src/main/java/com/geeky7/rohit/flash_a/SMSReceiver.java
@@ -0,0 +1,41 @@
+package com.geeky7.rohit.flash_a;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.geeky7.rohit.flash_a.activities.MainActivity;
+
+/**
+ * Created by Rohit on 8/07/2016.
+ */
+public class SMSReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+        String message = "";
+        String senderNum = "";
+        final Bundle bundle = intent.getExtras();
+        try {
+            if (bundle != null) {
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+                for (int i = 0; i < pdusObj.length; i++) {
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+                    senderNum = phoneNumber;
+                    message = currentMessage.getDisplayMessageBody();
+                    Log.i("SmsReceiver", "senderNum: " + senderNum + "; message: " + message);
+                }
+            }
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+        }
+        intent.putExtra("Message", message);
+        intent.putExtra("Sender", senderNum);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setClass(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+}
