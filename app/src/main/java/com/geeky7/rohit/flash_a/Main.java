c1332c96a2ea5790356e68ca5725614db31a7aba
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-04 08:22:59 +0930

    Main.java imported

---
+++ app/src/main/java/com/geeky7/rohit/flash_a/Main.java
@@ -0,0 +1,40 @@
+package com.geeky7.rohit.flash_a;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.widget.Toast;
+
+/**
+ * Created by Rohit on 9/08/2016.
+ */
+public class Main {
+
+    Context mContext;
+
+    public Main(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public static void showToast(String text) {
+        Toast.makeText(MyApplication.getAppContext(), text, Toast.LENGTH_SHORT).show();
+    }
+    public boolean showHomeScreen(){
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(startMain);
+        return true;
+    }
+    public boolean openLocationSettings(LocationManager service) {
+        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+        }
+        return enabled;
+    }
+}
