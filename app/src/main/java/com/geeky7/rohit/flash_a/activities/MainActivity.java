f8969e92b0e60606feff84e83ef177120de0a839
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-04 10:39:57 +0930

    MainActivity
    SMS permisssion added in the checkpermission;Tweaking

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/MainActivity.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/MainActivity.java
@@ -1,47 +1,54 @@
 package com.geeky7.rohit.flash_a.activities;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.geeky7.rohit.flash_a.Main;
 import com.geeky7.rohit.flash_a.R;
 import com.geeky7.rohit.flash_a.services.LocationService;
 
 public class MainActivity extends AppCompatActivity {
 
-    String message = "";
-    String sender = "";
+    String message = "No text";
+    String sender = "Empty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkPermission();
+//        checkPermission();
 
-//        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        boolean enabled = manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean enabled = manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             message = extras.getString("Message");
             sender = extras.getString("Sender");
-            Log.i("Text","Text is:"+ message);
+            Log.i("Message","Message is:"+ message);
 
             if ("Where".equals(message)) {
-//              if(message.contains("Where")){
-                Main.showToast("Location requested");
-                // this message initiated the location service which fetches the location and converts into an address
+//                Main.showToast("Location requested");
+                Log.i("Matched", "Location requested");
                 startService();
+                Log.i("LocationService", "Location Service initiated");
+
+//              if(message.contains("Where")){
+                // this message initiated the location service which fetches the location and converts into an address
             }
         }
 
     }
     public void checkPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_SMS},
+                0);
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                 0);
