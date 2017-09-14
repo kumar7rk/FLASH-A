d80d574e939abe5f8fff7bace74c88b027de882e
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-14 09:46:44 +0930

    Notification message when GPS is off;v1.1
    Tested;working fine

--- app/src/main/java/com/geeky7/rohit/flash_a/services/LocationService.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/services/LocationService.java
@@ -172,7 +172,7 @@ public class LocationService extends Service implements GoogleApiClient.OnConnec
                 else{
                     Log.i("Else", "gps off");
                     String name = getContactName(sender,getApplicationContext());
-                    pugNotification("Location Request from "+name," has requested your location","Click this notification to turn location on");
+                    pugNotification("Location Request from "+name,"Turn GPS on","");
                     getApplicationContext().registerReceiver(gpsReceiver,
                             new IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION));
                 }
@@ -245,21 +245,21 @@ public class LocationService extends Service implements GoogleApiClient.OnConnec
         if (noti)
             pugNotification("Location shared","Your current location shared with",name);
     }
-    public String getContactName(final String phoneNumber,Context context)
-    {
-        Uri uri=Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI,Uri.encode(phoneNumber));
+    public String getContactName(final String phoneNumber,Context context) {
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
 
         String[] projection = new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME};
 
-        String contactName="";
-        Cursor cursor=context.getContentResolver().query(uri,projection,null,null,null);
+        String contactName = "";
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
 
         if (cursor != null) {
-            if(cursor.moveToFirst()) {
-                contactName=cursor.getString(0);
-            cursor.close();
+            if (cursor.moveToFirst()) {
+                contactName = cursor.getString(0);
+                cursor.close();
+            }
         }
-        return contactName;
+            return contactName;
     }
 
     private String setAddress() {
@@ -288,7 +288,6 @@ public class LocationService extends Service implements GoogleApiClient.OnConnec
                 .largeIcon(R.drawable.cast_ic_notification_small_icon)
                 .flags(android.app.Notification.DEFAULT_ALL)
                 .simple()
-                .click(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), new Bundle())
                 .build();
     }
 }
