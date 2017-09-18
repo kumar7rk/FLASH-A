95a2b62d8369d2fd4617e28c6c321207b2397208
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-18 23:21:09 +0930

    A demo design activity added and given control of the app
    playing around with imageButtons in xml

---
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
@@ -0,0 +1,15 @@
+package com.geeky7.rohit.flash_a.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.geeky7.rohit.flash_a.R;
+
+public class Design extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.design);
+    }
+}
--- app/src/main/java/com/geeky7/rohit/flash_a/activities/MainActivity.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.design);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         final SharedPreferences.Editor editor = preferences.edit();
