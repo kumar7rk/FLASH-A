93d2c3a77d19d8288c25ed70adcaeb796e815368
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-20 00:15:13 +0930

    Setting onClickListener for serviceEnabled

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
@@ -1,15 +1,40 @@
 package com.geeky7.rohit.flash_a.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.geeky7.rohit.flash_a.Main;
 import com.geeky7.rohit.flash_a.R;
+
+import static com.geeky7.rohit.flash_a.R.id.serviceEnabled;
+import static com.geeky7.rohit.flash_a.R.id.serviceEnabled_lay;
+import static com.geeky7.rohit.flash_a.R.id.serviceEnabled_tv;
 
 public class Design extends AppCompatActivity {
 
+    LinearLayout serviceEnabled_lay;
+    TextView serviceEnabled_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.design);
+        serviceEnabled_lay = (LinearLayout)findViewById(serviceEnabled_lay);
+        serviceEnabled_tv = (TextView)findViewById(R.id.serviceEnabled_tv);
+
+        serviceEnabled.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                serviceEnabled();
+            }
+        });
+    }
+    public void serviceEnabled(){
+        Main.showToast("Service Enabled click");
+        serviceEnabled_lay.setBackgroundColor(Color.RED);
+        serviceEnabled_tv.setText("Service Disabled");
     }
 }
