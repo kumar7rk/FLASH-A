5b3bb00d075d0ee4b2ab5a0576485b811484e559
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-20 00:46:30 +0930

    playing around with serviceEnabled linear layout

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
@@ -4,28 +4,26 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.geeky7.rohit.flash_a.Main;
 import com.geeky7.rohit.flash_a.R;
-
-import static com.geeky7.rohit.flash_a.R.id.serviceEnabled;
-import static com.geeky7.rohit.flash_a.R.id.serviceEnabled_lay;
-import static com.geeky7.rohit.flash_a.R.id.serviceEnabled_tv;
 
 public class Design extends AppCompatActivity {
 
     LinearLayout serviceEnabled_lay;
     TextView serviceEnabled_tv;
+    ImageView serviceEnabled_iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.design);
-        serviceEnabled_lay = (LinearLayout)findViewById(serviceEnabled_lay);
+        serviceEnabled_lay = (LinearLayout)findViewById(R.id.serviceEnabled_lay);
         serviceEnabled_tv = (TextView)findViewById(R.id.serviceEnabled_tv);
+        serviceEnabled_iv = (ImageView)findViewById(R.id.serviceEnabled_iv);
 
-        serviceEnabled.setOnClickListener(new View.OnClickListener() {
+        serviceEnabled_lay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 serviceEnabled();
@@ -33,8 +31,8 @@ public class Design extends AppCompatActivity {
         });
     }
     public void serviceEnabled(){
-        Main.showToast("Service Enabled click");
         serviceEnabled_lay.setBackgroundColor(Color.RED);
-        serviceEnabled_tv.setText("Service Disabled");
+        serviceEnabled_tv.setText(R.string.service_disabled);
+        serviceEnabled_iv.setBackgroundResource(R.drawable.cast_ic_mini_controller_pause);
     }
 }
