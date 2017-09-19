89e191bc1c8d008e2a6c5168c761be995ad18830
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-20 01:43:58 +0930

    Stable version 4.0 :+1:
    Service funtionality applied on the new design; tested; running fine.
    Great job

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/Design.java
@@ -1,7 +1,9 @@
 package com.geeky7.rohit.flash_a.activities;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -15,24 +17,58 @@ public class Design extends AppCompatActivity {
     LinearLayout serviceEnabled_lay;
     TextView serviceEnabled_tv;
     ImageView serviceEnabled_iv;
+
+    SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.design);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        final boolean service = preferences.getBoolean("service",true);
+
         serviceEnabled_lay = (LinearLayout)findViewById(R.id.serviceEnabled_lay);
         serviceEnabled_tv = (TextView)findViewById(R.id.serviceEnabled_tv);
         serviceEnabled_iv = (ImageView)findViewById(R.id.serviceEnabled_iv);
+        if(service) enableService();
+        else disableService();
+    }
 
+    public void disableService(){
+        serviceEnabled_lay.setBackgroundColor(Color.RED);
+        serviceEnabled_tv.setText(R.string.service_disabled);
+        serviceEnabled_iv.setImageBitmap(null);
+        serviceEnabled_iv.setBackgroundResource(R.drawable.service_disabled);
+    }
+
+    public void enableService(){
+        serviceEnabled_lay.setBackgroundColor(0xffff8800);
+        serviceEnabled_tv.setText(R.string.service_enabled);
+        serviceEnabled_iv.setImageBitmap(null);
+        serviceEnabled_iv.setBackgroundResource(R.drawable.service_enabled);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final SharedPreferences.Editor editor = preferences.edit();
         serviceEnabled_lay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                serviceEnabled();
+                final boolean service = preferences.getBoolean("service",true);
+                if(service){
+                    editor.putBoolean("service",false);
+                    editor.commit();
+                    disableService();
+                }
+                else {
+                    editor.putBoolean("service",true);
+                    editor.commit();
+                    enableService();
+                }
             }
         });
-    }
-    public void serviceEnabled(){
-        serviceEnabled_lay.setBackgroundColor(Color.RED);
-        serviceEnabled_tv.setText(R.string.service_disabled);
-        serviceEnabled_iv.setBackgroundResource(R.drawable.cast_ic_mini_controller_pause);
     }
 }
