f965d0d9d674881b2bb48452e9fd0e9445efb7a8
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-20 20:32:06 +0930

    HomeAddress files added

---
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/HomeAddress.java
@@ -0,0 +1,15 @@
+package com.geeky7.rohit.flash_a.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.geeky7.rohit.flash_a.R;
+
+public class HomeAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_address);
+    }
+}
