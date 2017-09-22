5cdc809562ad2ff1e970211f9078bf05017db051
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-09-22 22:32:12 +0930

    FloatingActionButton added in home address

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/HomeAddress.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/HomeAddress.java
@@ -2,7 +2,9 @@ package com.geeky7.rohit.flash_a.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 
 import com.geeky7.rohit.flash_a.R;
@@ -11,15 +13,24 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
+
 public class HomeAddress extends AppCompatActivity {
 
     AutoCompleteTextView homeAddress;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_address);
-        openAutocompleteActivity();
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                m.usageAccessSettingsPage();
+                    openAutocompleteActivity();
+            }
+        });
     }
 
     private void openAutocompleteActivity() {
