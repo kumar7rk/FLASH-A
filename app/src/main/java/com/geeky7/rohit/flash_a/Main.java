01c93ee704f5ea09d43a35291218bdf02348860b
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-10-01 08:17:37 +1030

    Updated comments for Main class :tada:

--- app/src/main/java/com/geeky7/rohit/flash_a/Main.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/Main.java
@@ -1,11 +1,8 @@
 package com.geeky7.rohit.flash_a;
 
 import android.content.Context;
-import android.content.Intent;
-import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.provider.Settings;
 import android.widget.Toast;
 
 /**
@@ -19,26 +16,14 @@ public class Main {
         this.mContext = mContext;
     }
 
+
+    // This method pretty much works like the original method it is just that I don't have to pass all the parameters always
+    // and if I were to suppress any toast message in the final product, I could just comment the code in the method
+    // ez pz
     public static void showToast(String text) {
         Toast.makeText(MyApplication.getAppContext(), text, Toast.LENGTH_SHORT).show();
     }
-    public boolean showHomeScreen(){
-        Intent startMain = new Intent(Intent.ACTION_MAIN);
-        startMain.addCategory(Intent.CATEGORY_HOME);
-        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        mContext.startActivity(startMain);
-        return true;
-    }
-    public boolean openLocationSettings(LocationManager service) {
-        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        if (!enabled) {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(intent);
-        }
-        return enabled;
-    }
+    // checks if the internet is available on the device at the moment or not
     public boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) MyApplication.getAppContext().getSystemService(Context.CONNECTIVITY_SERVICE);
