d0a8f4f56f35030d67e92a59b91820989441d61e
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-10-01 08:21:35 +1030

    Updated comment for MyApplication with a disclamer :laughing:

--- app/src/main/java/com/geeky7/rohit/flash_a/MyApplication.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/MyApplication.java
@@ -1,3 +1,9 @@
+// this code is to get the context of the app which could be later given to any code, be it be an onclickListener
+// or anything where it is virtually impossible to get the context
+// I still remember when I discovered this code it was a revolution :)
+// Disclamer : if I insert :D instead of :), it either I am being lazy to reach parantheses
+// or I am just too happy. Mainly it's the former
+
 package com.geeky7.rohit.flash_a;
 
 import android.app.Application;
