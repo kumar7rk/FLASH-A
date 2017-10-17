592b4fa09604e2dcbedba261d5e45438aa4f5269
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-10-17 12:22:10 +1030

    A new class created for Constants

---
+++ app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
@@ -0,0 +1,10 @@
+package com.geeky7.rohit.flash_a;
+
+/**
+ * Created by Rohit on 17/10/2017.
+ */
+
+public class CONSTANT {
+
+    public static final String LOCATION_SERVICE = "LocationService;"
+}
