c70cab83787ddc71615bd79a779da3fd84478890
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-10-27 19:55:37 +1030

    New Constant values added

--- app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
@@ -9,5 +9,9 @@ public class CONSTANT {
     public static final String LOCATION_SERVICE = "LocationService";
     public static final String DESIGN= "DESIGN";
     public static final String HOME_ADDRESS= "HOME_ADDRESS ";
+    public static final String LATITUDE = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
+    public static final String ADDRESS = "ADDRESS";
+
 
 }
