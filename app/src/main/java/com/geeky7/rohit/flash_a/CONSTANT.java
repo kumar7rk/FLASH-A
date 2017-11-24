5543df01764dd8b4ae59f3d77932df187a3b3339
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-11-24 15:57:19 +1030

    space removed in home address constant value

--- app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
@@ -9,7 +9,7 @@ public class CONSTANT {
     public static final String LOCATION_SERVICE = "LocationService";
     public static final String LOCATION_SERVICE2 = "LocationService2";
     public static final String DESIGN= "DESIGN";
-    public static final String HOME_ADDRESS= "HOME_ADDRESS ";
+    public static final String HOME_ADDRESS = "HOME_ADDRESS";
     public static final String LATITUDE = "LATITUDE";
     public static final String LONGITUDE = "LONGITUDE";
     public static final String ADDRESS = "ADDRESS";
