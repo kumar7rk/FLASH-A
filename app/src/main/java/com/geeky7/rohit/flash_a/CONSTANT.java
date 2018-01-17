4ece3d57830a27238736ed072b8fd4fa5bee7025
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-01-17 12:00:39 +1030

    Arranging CONSTANT

--- app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/CONSTANT.java
@@ -6,24 +6,31 @@ package com.geeky7.rohit.flash_a;
 
 public class CONSTANT {
 
-    public static final String LOCATION_SERVICE = "LocationService";
-    public static final String LOCATION_SERVICE2 = "LocationService2";
+    public static final String LOCATION_SERVICE = "LOCATION_SERVICE";
+    public static final String LOCATION_SERVICE2 = "LOCATION_SERVICE2";
+    public static final String CONTACTS_FRAGMENT = "CONTACTS_FRAGMENT";
+    public static final String BACKGROUND_SERVICE = "BACKGROUND_SERVICE";
+
     public static final String DESIGN= "DESIGN";
+    public static final String SERVICE = "SERVICE";
     public static final String HOME_ADDRESS = "HOME_ADDRESS";
-    public static final String LATITUDE = "LATITUDE";
-    public static final String LONGITUDE = "LONGITUDE";
+    public static final String KEYWORD = "KEYWORD";
+
     public static final String ADDRESS = "ADDRESS";
     public static final String PLACE_NAME = "PLACE_NAME";
-    public static final String SERVICE = "SERVICE";
+
+    public static final String LATITUDE = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
+
+
     public static final String SENDER = "SENDER";
     public static final String MESSAGE = "MESSAGE";
-    public static final String KEYWORD = "KEYWORD";
-    public static final String BACKGROUND_SERVICE = "BACKGROUND_SERVICE";
+
     public static final String APP_OPENED_FIRST_TIME = "APP_OPENED_FIRST_TIME";
     public static final String NO_INTERNET = "NO_INTERNET";
     public static final String SELECTED_CONTACTS = "SELECTED_CONTACTS";
     public static final String SELECTED_CONTACTS_INDEX = "SELECTED_CONTACTS_INDEX";
-    public static final String CONTACTS_FRAGMENT = "ContactsFragment";
+
 
 
 }
