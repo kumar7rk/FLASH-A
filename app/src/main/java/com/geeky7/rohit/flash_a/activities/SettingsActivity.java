6ec9c85ad1b30f140dbbc81635ce7f0c53fd6577
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-01-28 09:47:56 +1030

    SettingsActivity: CleanUp Code :recycle: :green_heart:

--- app/src/main/java/com/geeky7/rohit/flash_a/activities/SettingsActivity.java
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/SettingsActivity.java
@@ -1,4 +1,5 @@
 // new written
+// shows lists and pretty much anything else for a settingsActivity
 package com.geeky7.rohit.flash_a.activities;
 
 import android.content.SharedPreferences;
@@ -26,10 +27,12 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Sha
         // adds a fragment which extract a preference screen
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
 
+        // adding support for actionbar back button at the top
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    // apparently this method is not being called
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
         Main.showToast("I'm called " + s);
@@ -38,13 +41,18 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Sha
         else Main.showToast("eta off");
     }
 
+    // a new fragment inner class which contains the layout
     public static class SettingsFragment extends PreferenceFragment {
+        // toggles and changes summary Select Contacts preference
+        //
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
+            final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
             // getting the preference data from xml
             addPreferencesFromResource(R.xml.settings);
-            final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
             final Preference eta = findPreference("Settings_Send_ETA");
             final Preference contacts = findPreference("Settings_Select_Contacts");
@@ -53,8 +61,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Sha
             if (!b) contacts.setEnabled(false);
             if (b) contacts.setEnabled(true);
 
-            setSummary(preferences, contacts);
+            setSelectContactsSummary(preferences, contacts);
 
+            // eta toggles- enable/disable select contacts
             eta.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object o) {
@@ -64,34 +73,39 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Sha
                     return true;
                 }
             });
+            // onLick Select Contacts shows contactsFragment
+            // returning true means that we have handle the onClick event
             contacts.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
                     ContactsFragment contact = new ContactsFragment();
                     if (!contact.isAdded())
                         contact.show(getFragmentManager(),"Contacts");
-                    setSummary(preferences,contacts);
-                    return false;
+                    setSelectContactsSummary(preferences,contacts);
+                    return true;
                 }
             });
-
         }
 
-        private void setSummary(SharedPreferences preferences, Preference contacts) {
+        // change the summary for select contacts when the chosen contacts are updated
+        private void setSelectContactsSummary(SharedPreferences preferences, Preference contacts) {
             Set<String> set = new HashSet<>();
             set = preferences.getStringSet(CONSTANT.SELECTED_CONTACTS,set);
             String s1 = "";
 
+            // combine the selected contacts in a string
             for (String s : set)
                 s1 +=s+", ";
 
+            // if no contact is selected show "None"
             if (set.size()==0) s1 = "None";
+            // remove the last comma when adding contact in the string
             else if (s1.endsWith(", "))
                 s1 = s1.substring(0,s1.length()-2);
-
             contacts.setSummary(s1);
         }
     }
+    // Beam me back, Scotty to Design activity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId())
