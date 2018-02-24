dbe9c9f563275cb58e29cedc4c962f8d7bbbd441
Rohit Kumar <rohitkumarrk1992@gmail.com>
2018-02-24 13:48:51 +1030

    CurrentLocationMapActivity added with xml and manifest

---
+++ app/src/main/java/com/geeky7/rohit/flash_a/activities/currentLocationMapActivity.java
@@ -0,0 +1,13 @@
+package com.geeky7.rohit.flash_a.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class currentLocationMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_location_map);
+    }
+}
